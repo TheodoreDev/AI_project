3cd7fbce3795f068df8c813f0ee5ed4a3d0258ae
ThéodoreDev <theodore.delbove@gmail.com>
2024-03-02 18:29:39 +0100

    Create the ai classifier model

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
-import os
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 from tensorflow.keras import datasets, layers, models
+import pathlib
 
 (training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.load_data()
 training_images, testing_images = training_images / 255, testing_images / 255
@@ -24,16 +23,44 @@ training_labels = training_labels[:20000]
 testing_images = testing_images[:4000]
 testing_labels = testing_labels[:4000]
 
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)))
-model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Conv2D(64, (3,3), activation='relu'))
-model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Conv2D(64, (3,3), activation='relu'))
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10, activation='softmax'))
+#model = models.Sequential()
+#model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)))
+#model.add(layers.MaxPooling2D((2,2)))
+#model.add(layers.Conv2D(64, (3,3), activation='relu'))
+#model.add(layers.MaxPooling2D((2,2)))
+#model.add(layers.Conv2D(64, (3,3), activation='relu'))
+#model.add(layers.Flatten())
+#model.add(layers.Dense(64, activation='relu'))
+#model.add(layers.Dense(10, activation='softmax'))
+#
+#model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+#
+#model.fit(training_images, training_labels, epochs=10, validation_data=(testing_images, testing_labels))
+#
+#loss, accuracy = model.evaluate(testing_images, testing_labels)
+#print(f"loss : {loss}")
+#print(f"accuracy : {accuracy}")
+#
+#model.save('image_classifier.h5')
 
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model = models.load_model('image_classifier.h5')
 
-model.fit(training_images, training_labels, epochs=10, validation_data=(testing_images, testing_labels))
+img_count = int(0)
+for path in pathlib.Path('img').iterdir():
+    img_count = img_count + 1
+    print(img_count)
+
+for i in range(img_count):
+    plt.subplot(img_count, img_count, i + 1)
+    plt.xticks([])
+    plt.yticks([])
+    img = cv.imread(f'img/{i+1}.jpg')
+    img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    prediction = model.predict(np.array([img]) / 255)
+    index = np.argmax(prediction)
+    plt.xlabel(class_names[index])
+    print(f"Prediction is {class_names[index]}")
+
+plt.show()
